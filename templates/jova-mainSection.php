4a71c6e73a5ccfcd60e43010ee61d39c4abdb2c4
96vlada <34660711+96vlada@users.noreply.github.com>
2020-12-21 20:01:32 +0100

    Add files via upload

---
+++ templates/jova-mainSection.php
@@ -0,0 +1,21 @@
+<div class="container-fluid p-0">
+  <div class="row j-c-c">
+    <div class="col-12">
+
+      <div class="jova-main-area txt-center" style="background-image: url(<?php echo get_the_post_thumbnail_url(get_option('page_on_front')); ?>);">
+        <div class='btn-cont'>
+          <a class='btn' href='#'>
+            Shop now
+            <span class='line-1'></span>
+            <span class='line-2'></span>
+            <span class='line-3'></span>
+            <span class='line-4'></span>
+          </a>
+        </div>
+
+      </div>
+
+
+    </div>
+  </div>
+</div>
